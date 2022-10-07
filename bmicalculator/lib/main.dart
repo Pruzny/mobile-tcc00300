082cca3bfd57558f15fc2d2383a7e22f9b239e0a
Maximilian Harrisson <maximilianharrisson@gmail.com>
2022-10-07 13:58:56 -0300

    Add calculate function and update style

--- bmicalculator/lib/main.dart
+++ bmicalculator/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -12,10 +12,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static const double widgetWidth = 200.0;
   static const Padding space = Padding(padding: EdgeInsets.only(top: 16));
-  double _weight = 0;
-  double _height = 0;
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  Widget _result = Text(
+    "Write your height and weight",
+    style: Styles.resultText,
+    textAlign: TextAlign.center,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -37,78 +41,153 @@ class _MyAppState extends State<MyApp> {
           child: Center(child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                decoration: Styles.inputBox,
-                padding: EdgeInsets.only(left: 20),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: "Weight",
-                    labelStyle: Styles.placeholderText,
+              FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Container(
+                  decoration: Styles.widgetBox,
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: [
+                      Container(
+                          width: double.infinity,
+                          decoration: Styles.inputBox,
+                          padding: EdgeInsets.only(left: 20),
+                          child: TextField(
+                            controller: _weightController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: "Weight",
+                              labelStyle: Styles.labelText,
+                              hintText: "Ex: 59.9",
+                              hintStyle: Styles.placeholderText,
+                              focusColor: Color.fromRGBO(255, 128, 48, 1),
+                              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(
+                                color: Color.fromRGBO(255, 128, 48, 1),
+                                width: 2,
+                              )),
+                            ),
+                            cursorColor: Color.fromRGBO(255, 128, 48, 1),
+                            style: Styles.placeholderText,
+                          )
+                        ),
+                      space,
+                      Container(
+                          decoration: Styles.inputBox,
+                          padding: EdgeInsets.only(left: 20),
+                          child: TextField(
+                            controller: _heightController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: "Height",
+                              labelStyle: Styles.labelText,
+                              hintText: "Ex: 1.68",
+                              hintStyle: Styles.placeholderText,
+                              focusColor: Color.fromRGBO(255, 128, 48, 1),
+                              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(
+                                color: Color.fromRGBO(255, 128, 48, 1),
+                                width: 2,
+                              )),
+                            ),
+                            cursorColor: Color.fromRGBO(255, 128, 48, 1),
+                            style: Styles.placeholderText,
+                          )
+                        ),
+                      space,
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: calculateBmi,
+                          style: Styles.calculateButton,
+                          child: Text(
+                            "Calculate",
+                            style: Styles.calculateText,
+                          )
+                        ),
+                      ),
+                    ],
                   ),
-                  style: Styles.placeholderText,
-                )
+                ),
               ),
               space,
-              Container(
-                decoration: Styles.inputBox,
-                padding: EdgeInsets.only(left: 20),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: "Height",
-                    labelStyle: Styles.placeholderText,
-                  ),
-                  style: Styles.placeholderText,
-                )
-              ),
-              space,
-              SizedBox(
-                width: widgetWidth,
-                child: ElevatedButton(
-                  onPressed: calculateBmi,
-                  style: Styles.calculateButton,
-                  child: Text(
-                    "Calculate",
-                    style: Styles.calculateText,
-                  )),
-              )
+              _result,
             ],
           )),
         ),
       ),
     );
   }
-
   void calculateBmi() {
-
+    setState(() {
+      String? weight = _weightController.text;
+      String? height = _heightController.text;
+      if (weight.isNotEmpty && height.isNotEmpty) {
+        _result = Container(
+          decoration: Styles.widgetBox,
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: [
+              Text(
+                "Your BMI is\n${(double.parse(weight)/pow(double.parse(height), 2)).toStringAsFixed(2)}",
+                style: Styles.resultText,
+                textAlign: TextAlign.center,
+              ),
+              space,
+              ElevatedButton(
+                onPressed: () {},
+                style: Styles.shareButton,
+                child: Text("Share"),
+              ),
+            ],
+          )
+        );
+      }
+    });
   }
 }
 
 class Styles {
   static const TextStyle appTitle = TextStyle(
     fontSize: 30,
-    color: Colors.purpleAccent,
+    color: Color.fromRGBO(240, 114, 65, 1),
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle placeholderText = TextStyle(
+  static const TextStyle labelText = TextStyle(
     fontSize: 30,
+    color: Colors.grey,
+  );
+  static const TextStyle placeholderText = TextStyle(
+    fontSize: 20,
     color: Colors.grey,
   );
   static const BoxDecoration mainBox = BoxDecoration(
     color: Colors.black,
+  );
+  static BoxDecoration widgetBox = BoxDecoration(
+    color: Color.fromRGBO(16, 16, 16, 1),
+    borderRadius: BorderRadius.circular(30),
   );
   static BoxDecoration inputBox = BoxDecoration(
     color: Color.fromRGBO(32, 32, 32, 1),
     borderRadius: BorderRadius.circular(30),
   );
   static ButtonStyle calculateButton = ButtonStyle(
-    backgroundColor: MaterialStateProperty.all(Colors.purpleAccent),
+    backgroundColor: MaterialStateProperty.all(Color.fromRGBO(192, 72, 72, 1)),
+    minimumSize: MaterialStateProperty.all(Size(double.infinity, 60)),
     shape: MaterialStateProperty.all(RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(20),
+      borderRadius: BorderRadius.circular(30),
+    )),
+  );
+  static ButtonStyle shareButton = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all(Color.fromRGBO(192, 72, 72, 1)),
+    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30),
     )),
   );
   static TextStyle calculateText = TextStyle(
+    color: Colors.white,
+    fontSize: 32,
+  );
+  static TextStyle resultText = TextStyle(
     color: Colors.white,
     fontSize: 32,
   );
