a35971a4bd88ad43d83869de7e1f3aae526bed1d
Maximilian Harrisson <maximilianharrisson@gmail.com>
2022-12-04 14:17:31 -0300

    Start marketplace

---
+++ marketplace/lib/pages/Home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Marketplace"),
+        centerTitle: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: const Text("Login"),
+      ),
+    );
+  }
+}
